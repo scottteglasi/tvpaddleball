4906256ceb9e2f5f3ae09be344f297ae3c2c5533
Scott Teglasi <scott.teglasi@gmail.com>
2019-01-17 17:42:46 -0500

    Add scoring, bounce counting, "nice spin".  Add nosleep.js to keep controller phone from sleeping

--- public/gameboard/game.js
+++ public/gameboard/game.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     physics: {
         default: 'matter',
         matter: {
@@ -23,9 +23,22 @@ var socket = io('http://' + window.location.hostname + ':3000');
 var leftPaddle;
 var rightPaddle;
 var paddleYmin = 0;
-var paddleYmax = 600;
+var paddleYmax = 720;
 
 var ball;
+
+var playerScores = { "1": 0, "2": 0 };
+var scoreText;
+
+var lastAngularVelocity;
+var niceSpinText;
+
+var bounceCount = 0;
+var highestBounceCount = 0;
+var bounceCountText;
+var highestBounceCountText;
+
+var lastBallVelocity = 0;
 
 
 function preload() {
@@ -38,11 +51,17 @@ function preload() {
 function create() {
     this.matter.world.setBounds(-20, -20, game.config.width+50, game.config.height+20);
     this.matter.world.setGravity(0,0);
+    scoreText = this.add.text(640,20, '0 : 0', { fontSize: '32px', fill: '#FFF' });
+    niceSpinText = this.add.text(640,360,'NICE SPIN', { fontSize: '50px', fill: '#FF0' });
+    niceSpinText.setVisible(false);
 
-    leftPaddle = this.matter.add.sprite(100,300,'paddle');
+    bounceCountText = this.add.text(640, 360, '0', { fontSize: '96px', fill: '#555' });
+    highestBounceCountText = this.add.text(640, 500, '0', { fontSize: '72px', fill: '#444' });
+
+    leftPaddle = this.matter.add.sprite(100, 360, 'paddle');
     leftPaddle.setStatic(true);
 
-    rightPaddle = this.matter.add.sprite(700, 300, 'paddle');
+    rightPaddle = this.matter.add.sprite(1180, 360, 'paddle');
     rightPaddle.setStatic(true);
 
     socket.on('controller_data_1', function(data) {
@@ -68,13 +87,28 @@ function create() {
         rightPaddle.setY(rightPaddlePosition);
         rightPaddle.setRotation(data.do.gamma * Math.PI / 180);
     });
-    ball = this.matter.add.sprite(400, 300, 'ball');
+    ball = this.matter.add.sprite(640, 360, 'ball');
     ball.setVelocity(5, 0);
     ball.setFriction(0, 0);
     ball.setBounce(2);
+
 }
 
 function update() {
+    if (lastBallVelocity === 0)
+    {
+        lastBallVelocity = ball.body.velocity.x;
+    }
+
+    if (lastBallVelocity !== ball.body.velocity.x)
+    {
+        if ( (lastBallVelocity > 0 && ball.body.velocity.x < 0) || (lastBallVelocity < 0 && ball.body.velocity.x > 0))
+        {
+            incrementBounceCount();
+            lastBallVelocity = ball.body.velocity.x;
+        }
+    }
+
     if (ball.body.velocity.x > 10)
     {
         ball.setVelocityX(10);
@@ -86,19 +120,61 @@ function update() {
 
     if (ball.x >= game.config.width) {
         // player 1 scores
-        console.log('Player 1 scores');
-        ball.setX(400);
-        ball.setY(300);
-        ball.setVelocity(-5,0);
-        ball.setAngularVelocity(0);
+        playerScored(1);
+
     } else if (ball.x <= 0) {
-        // player 2 scores
-        console.log('Player 2 scores');
-        ball.setX(400);
-        ball.setY(300);
-        ball.setVelocity(5,0);
-        ball.setAngularVelocity(0);
+        playerScored(2);
     }
 
+    if (lastAngularVelocity !== ball.body.angularVelocity)
+    {
+        console.log(ball.body.angularVelocity);
+
+        if (ball.body.angularVelocity > 0.5 || ball.body.angularVelocity < -0.5)
+        {
+            niceSpinText.setVisible(true);
+        } else {
+            niceSpinText.setVisible(false);
+        }
+    }
+
+    lastAngularVelocity = ball.body.angularVelocity;
+}
+
+function incrementBounceCount()
+{
+    bounceCount++;
+    bounceCountText.setText(bounceCount);
+    if (bounceCount > highestBounceCount)
+    {
+        highestBounceCount++;
+        highestBounceCountText.setText(highestBounceCount);
+    }
+}
+
+function resetBounceCount()
+{
+    bounceCount = 0;
+    bounceCountText.setText(bounceCount);
+    lastBallVelocity = 0;
+}
+
+function playerScored(scoringPlayerNumber)
+{
+    playerScores[scoringPlayerNumber]++;
+    renderScores();
+
+    var ballVelocityX = scoringPlayerNumber === 1 ? -5 : 5;
+
+    ball.setX(640);
+    ball.setY(360);
+    ball.setVelocity(ballVelocityX,0);
+    ball.setAngularVelocity(0);
+    resetBounceCount();
+}
+
+function renderScores()
+{
+    scoreText.setText(playerScores[1] + ' : ' + playerScores[2]);
 }
 
